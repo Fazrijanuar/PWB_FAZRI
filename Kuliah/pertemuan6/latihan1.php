9517232981f2fe387c96cdd57247fa1bc278e4e8
fazrijanuar <fazrijanuar@gmail.com>
2022-01-22 18:38:35 +0700

    Memasukkan file baru

--- Kuliah/pertemuan6/latihan1.php
+++ Kuliah/pertemuan6/latihan1.php
@@ -1,14 +1,12 @@
-<?php  
-// hostname or ip of server
-$servername='localhost';
-
-// username and password
-$dbusername='root';
-$dbpassword='';
-$link=mysql connect("$servername","$dbusername","$dbpassword")
-or die ("koneksi database gagal");
-if ($link)
-{
-    echo "Koneksi Berhasil";
+<?php
+    // hostname or ip of server
+    $servername='localhost';
+    // username and password
+    $dbusername='root';
+    $dbpassword='';
+    $link=mysqli_connect("$servername","$dbusername","$dbpassword")
+    or die ( "Koneksi database gagal");
+    if ($link)
+    {
+    echo "Koneksi berhasil";
 }
-?>
